138147cab17b72c34162022d78bfe73267a01736
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-08 21:01:23 -0400

    experimented with simplecontoller

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -2,9 +2,24 @@
   angular.module('ngmongoblog')
          .controller('SimpleController', SimpleController);
 
-  SimpleController.$inject = ['$scope' ]
+  SimpleController.$inject = ['$scope', 'UserService']
 
-  function SimpleController($scope){
+  function SimpleController($scope, UserService){
     $scope.message = "Yeaaaahhh!!!";
+
+    UserService.getAllUsers()
+               .then(function(response){
+                 console.log(response);
+               });
+    var userObj = {
+      firstName: 'Lil',
+      lastName: 'Jon',
+      age: 41,
+      email: 'love@hate.com'
+    }
+    UserService.createUser(userObj)
+               .then(function(response){
+                 console.log(response);
+               })
   }
 }());
