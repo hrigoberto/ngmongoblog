4cbeb8f627b95fac509a0be3db2db14683a89cd0
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-09 18:50:10 -0400

    finished UserService and tested it

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -9,17 +9,36 @@
 
     UserService.getAllUsers()
                .then(function(response){
-                 console.log(response);
+                 console.log(response, 'ggt all users');
                });
     var userObj = {
       firstName: 'Lil',
       lastName: 'Jon',
       age: 41,
-      email: 'love@hate.com'
+      email: 'lohaate.com'
+    }
+    var userId = "57aa5968edc0cf1100d893fb"
+    var updateInfo = {
+      firstName: 'Big',
+      lastName: 'Jon',
+      age: 41,
+      email: 'lofagadgfe@hate.com'
     }
     UserService.createUser(userObj)
                .then(function(response){
-                 console.log(response);
+                 console.log(response, 'created user');
                })
-  }
+    UserService.getOneUser(userId)
+               .then(function(response){
+                 console.log(response, "got one user");
+               });
+    UserService.updateUser(userId, updateInfo)
+               .then(function(response){
+                 console.log(response, "updated user");
+               })
+    UserService.deleteUser(userId)
+               .then(function(response){
+                 console.log(response, "deleted user");
+               })
+   }
 }());
